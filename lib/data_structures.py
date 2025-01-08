b52b7636b0d5ce97bb02446ff4af5aa4fc9d6601
enock-jufri <enockjufri@gmail.com>
2025-01-08 19:24:05 +0300

    manipulating list and dict using methods

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,31 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food.get("name") for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food.get("heat_level") > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
-
+    for food in spicy_foods:
+        print(f"{food.get("name")} ({food.get("cuisine")}) | Heat Level: {food.get("heat_level")* '🌶'}")
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
-
+    for food in spicy_foods:
+        if(food.get("cuisine")==cuisine):
+            return food
 def print_spiciest_foods(spicy_foods):
-    pass
-
+    print_spicy_foods(get_spiciest_foods(spicy_foods))
 def get_average_heat_level(spicy_foods):
-    pass
-
+    total=0
+    for food in spicy_foods:
+        total += food.get("heat_level")
+    average=total/len(spicy_foods)
+    return int(average)
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+print(create_spicy_food(spicy_foods,{
+        'name': 'Griot',
+        'cuisine': 'Haitian',
+        'heat_level': 10,
+    }))
